adad549b1cca89c162830452cf06a28675f7c9c1
jordan-lenchitz <jordan.lenchitz@gmail.com>
2026-06-06 05:57:50 +0000

    debug: sclang version check in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ echo "ENTRYPOINT: Starting Xvfb..."
 Xvfb :99 -screen 0 1024x768x24 &
 export DISPLAY=:99
 
+echo "ENTRYPOINT: sclang version check..."
+sclang -v
+
 echo "ENTRYPOINT: Starting services..."
 # Start the FastAPI backend in the background
 python3 python/sc_backend.py &
